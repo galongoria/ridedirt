af27b7790b16cda85f7a99cbe162a3db902732bd
galongoria <george.longoria@utexas.edu>
2022-09-28 09:52:06 -0500

    added more searching tools for fb

--- code/facebook_api.py
+++ code/facebook_api.py
@@ -15,20 +15,6 @@ load_dotenv()
 token = os.getenv("token")
 
 
-def get_fb_groups():
-
-    return {
-    'Antills': {'id':'534322359952442', 'location': '1001 S Dairy Ashford Rd, Houston, TX 77077'},
-    'Cypress Creek Trails': {'id': '160122897466548', 'location' : '14234 W Cypress Forest Dr, Houston, TX 77070'},
-    'Memorial Park Trails': {'id': '134220983339617', 'location': 'N Picnic Ln, Houston, TX 77007'},
-    'Brazos River Trails': {'id': '157990614387212', 'location': '8125 Homeward Way, Sugar Land, TX 77479'},
-    'Pearland MTB Trail': {'id': '768933773193257', 'location': 'Province Village Dr, Pearland, TX 77581'},
-    'Fonteno Park Trails': {'id': '667149480730957', 'location': '14350 1/2 Wallisville Rd, Houston, TX 77049'},
-    'League City Trails':{'id': '1417274005224393', 'location': '100 Alderwood St, League City, TX 77573'},
-    'Bridgeland Trails': {'id': '1034914603303904', 'location': '18310 House Hahl Rd, Cypress, TX 77433'},
-    'Chisenhall Trails': {'id':'579687176161531', 'location': '500 W Hidden Creek Pkwy, Burleson, TX 76028'},
-    }
-
 def get_group_picture(group_id):
 
     """Input the Facebook Group ID and output the picture url, group name, and time of request"""
@@ -74,13 +60,14 @@ def compare_all():
     d = {}
 
     for trail, value in group_dict.items():
-        print(f'Looking at {trail}')
-        try:
-            name, datetime_string, pic = get_group_picture(str(value['id']))
-            abspath = save_image(name, datetime_string, pic)
-            d[name] = compare_picture(abspath, name)
-        except facebook.GraphAPIError as error:
-            d[name] = 'check_id'
+        if value['group_photo']:
+            print(f'Looking at {trail}')
+            try:
+                name, datetime_string, pic = get_group_picture(str(value['id']))
+                abspath = save_image(name, datetime_string, pic)
+                d[name] = compare_picture(abspath, name)
+            except facebook.GraphAPIError as error:
+                d[name] = 'check_id'
     return d
 
 def get_static():
@@ -91,12 +78,13 @@ def get_static():
     l = []
 
     for trail, value in group_dict.items():
-        print(f'Looking at {trail}')
-        try:
-            name, datetime_string, pic = get_group_picture(str(value['id']))
-            l.append((trail, name, value['id'], f'https://www.facebook.com/groups/{value["id"]}',value['location'],''))
-        except facebook.GraphAPIError as error:
-            print('!!! Check_id !!!')
+        if value['group_photo']:
+            print(f'Looking at {trail}')
+            try:
+                name, datetime_string, pic = get_group_picture(str(value['id']))
+                l.append((trail, name, value['id'], f'https://www.facebook.com/groups/{value["id"]}',value['location'],''))
+            except facebook.GraphAPIError as error:
+                print('!!! Check_id !!!')
     return l
 
 
--- code/searches.py
+++ code/searches.py
@@ -1,13 +1,80 @@
 def get_fb_groups():
 
-	return {
-	'Antills': {'id':'534322359952442', 'location': '1001 S Dairy Ashford Rd, Houston, TX 77077'},
-	'Cypress Creek Trails': {'id': '160122897466548', 'location' : '14234 W Cypress Forest Dr, Houston, TX 77070'},
-	'Memorial Park Trails': {'id': '134220983339617', 'location': 'N Picnic Ln, Houston, TX 77007'},
-	'Brazos River Trails': {'id': '157990614387212', 'location': '8125 Homeward Way, Sugar Land, TX 77479'},
-	'Pearland MTB Trail': {'id': '768933773193257', 'location': 'Province Village Dr, Pearland, TX 77581'},
-	'Fonteno Park Trails': {'id': '667149480730957', 'location': '14350 1/2 Wallisville Rd, Houston, TX 77049'},
-	'League City Trails':{'id': '1417274005224393', 'location': '100 Alderwood St, League City, TX 77573'},
-	'Bridgeland Trails': {'id': '1034914603303904', 'location': '18310 House Hahl Rd, Cypress, TX 77433'},
-	'Chisenhall Trails': {'id':'579687176161531', 'location': '500 W Hidden Creek Pkwy, Burleson, TX 76028'},
-	}
+    return {
+        "Antills": {
+            "group_photo": True,
+            "scrape": False,
+            "scrape_url": None,
+            "id": "534322359952442",
+            "location": "1001 S Dairy Ashford Rd, Houston, TX 77077",
+        },
+        "Cypress Creek Trails": {
+            "group_photo": True,
+            "scrape": False,
+            "scrape_url": None,
+            "id": "160122897466548",
+            "location": "14234 W Cypress Forest Dr, Houston, TX 77070",
+        },
+        "Memorial Park Trails": {
+            "group_photo": True,
+            "scrape": False,
+            "scrape_url": None,
+            "id": "134220983339617",
+            "location": "N Picnic Ln, Houston, TX 77007",
+        },
+        "Brazos River Trails": {
+            "group_photo": True,
+            "scrape": False,
+            "scrape_url": None,
+            "id": "157990614387212",
+            "location": "8125 Homeward Way, Sugar Land, TX 77479",
+        },
+        "Pearland MTB Trail": {
+            "group_photo": True,
+            "scrape": False,
+            "scrape_url": None,
+            "id": "768933773193257",
+            "location": "Province Village Dr, Pearland, TX 77581",
+        },
+        "Fonteno Park Trails": {
+            "group_photo": True,
+            "scrape": False,
+            "scrape_url": None,
+            "id": "667149480730957",
+            "location": "14350 1/2 Wallisville Rd, Houston, TX 77049",
+        },
+        "League City Trails": {
+            "group_photo": True,
+            "scrape": False,
+            "scrape_url": None,
+            "id": "1417274005224393",
+            "location": "100 Alderwood St, League City, TX 77573",
+        },
+        "Bridgeland Trails": {
+            "group_photo": True,
+            "scrape": False,
+            "id": "1034914603303904",
+            "location": "18310 House Hahl Rd, Cypress, TX 77433",
+        },
+        "Chisenhall Trails": {
+            "group_photo": True,
+            "scrape": False,
+            "scrape_url": None,
+            "id": "579687176161531",
+            "location": "500 W Hidden Creek Pkwy, Burleson, TX 76028",
+        },
+        "Coler MTB Preserve": {
+            "group_photo": False,
+            "scrape": True,
+            "scrape_url": "https://www.facebook.com/groups/262979124112610/announcements",
+            "id": "262979124112610",
+            "location": "",
+        },
+        # "Dana Peak Trails": {
+        #     "group_photo": False,
+        #     "scrape": True,
+        #     "scrape_url": "https://www.facebook.com/groups/2653095754965458",
+        #     "id": "2653095754965458",
+        #     "location": "",
+        # },
+    }
